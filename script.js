6c9587e34f8917816342c8e87c2a4d177eadae43
Abhishek Lokhande <115551272+AAbhishekLokhande@users.noreply.github.com>
2023-10-29 15:43:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function getFormvalue() {
-    //Write your code here
-	let output = document.getElementsByName("fname");
-	let output2 = document.getElementsByName("lname");
-	alert(output+ " " +output2);
+ //    //Write your code here
+	// let output = document.getElementsByName("fname");
+	// let output2 = document.getElementsByName("lname");
+	// alert(output+ " " +output2);
+	var firstName = document.getElementById('form1').elements['fname'].value;
+    var lastName = document.getElementById('form1').elements['lname'].value;
+    alert("First Name: " + firstName + "\nLast Name: " + lastName);
+    return false; /
 }
