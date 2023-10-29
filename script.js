32d7257b04b9b4f35c2becfb2695744adef7527e
Abhishek Lokhande <115551272+AAbhishekLokhande@users.noreply.github.com>
2023-10-29 15:36:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let output = document.getElementsByName("fname");
+	let output2 = document.getElementsByName("lname");
+	alert(output+ " " +output2);
 }
